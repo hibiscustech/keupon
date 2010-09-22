9dfeaed35b7f748675c4c94bbcb9c6c7073545fd
hibiscustech <relton@hibiscustech.com>
2010-09-22 12:19:12 +0530

    customer_deals table alter

--- db/migrate/20100922064043_alter_customer_deals_table_add_status_and_deal_code_columns.rb
+++ db/migrate/20100922064043_alter_customer_deals_table_add_status_and_deal_code_columns.rb
@@ -3,7 +3,8 @@ class AlterCustomerDealsTableAddStatusAndDealCodeColumns < ActiveRecord::Migrati
     #execute %Q{ ALTER TABLE `deals` MODIFY COLUMN `name` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL }
 
     execute %Q{ ALTER TABLE `customer_deals` ADD COLUMN `status` ENUM('new','available','used','expired','failed') NOT NULL DEFAULT 'new' AFTER `quantity`,
-                ADD COLUMN `deal_code` VARCHAR(25) NOT NULL AFTER `status` }
+                ADD COLUMN `deal_code` VARCHAR(25) default NULL AFTER `status` }
+
   end
 
   def self.down
