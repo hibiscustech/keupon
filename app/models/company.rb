2353131d6c2aa56eddf289ae87b11effa0697965
hibiscustech <relton@hibiscustech.com>
2010-10-22 13:05:07 +0530

    demand deals

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,9 @@
 class Company < ActiveRecord::Base
 
    belongs_to :merchant_profile
-   
+
+   has_attached_file :company_photo,
+    :styles => {
+      :thumb=> "100x100#",
+      :small  => "150x150>" }
 end
---
+++ db/migrate/20101022071732_alter_companies_deals_customer_demand_deals_tables.rb
@@ -0,0 +1,15 @@
+class AlterCompaniesDealsCustomerDemandDealsTables < ActiveRecord::Migration
+  def self.up
+    execute %Q{ ALTER TABLE `customer_demand_deals` MODIFY COLUMN `status` ENUM('new','offered','cancelled','accepted','offered') NOT NULL DEFAULT 'new'}
+
+    execute %Q{ ALTER TABLE `deals` ADD COLUMN `keupoints_required` INT(11) UNSIGNED AFTER `deal_photo_file_size` }
+
+    execute %Q{ ALTER TABLE `companies` DROP COLUMN `logo`,
+                 ADD COLUMN `company_photo_file_name` VARCHAR(255) AFTER `merchant_profile_id`,
+                 ADD COLUMN `company_photo_content_type` VARCHAR(255) AFTER `company_photo_file_name`,
+                 ADD COLUMN `company_photo_file_size` INT(11) UNSIGNED AFTER `company_photo_content_type` }
+  end
+
+  def self.down
+  end
+end
