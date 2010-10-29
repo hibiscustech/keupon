f0c4851bd88bcc8b3f00a97a54bd3a563d8ae273
hibiscustech <relton@hibiscustech.com>
2010-10-29 18:49:10 +0530

    purchasing deals

--- app/models/constant.rb
+++ app/models/constant.rb
@@ -1,2 +1,6 @@
 class Constant < ActiveRecord::Base
+  def self.dollar_to_keupoint_convertion
+    query = %Q{ select value from constants where name = 'keupoint'}
+    find_by_sql(query)[0].value.to_f
+  end
 end
