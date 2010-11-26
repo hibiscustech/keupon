9fd55a1ad1d3a5d3f23248d6401110383380d4c2
hibiscustech <relton@hibiscustech.com>
2010-11-26 14:30:30 +0530

    new deal

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -430,7 +430,7 @@ class MerchantController < ApplicationController
     @deal = Deal.find(params[:id])
     schedule = @deal.deal_schedule
     preference = ((@deal.preferred.blank? || @deal.preferred.to_s == "0") && params[:preferred] == "1")? true : false
-    un_preference = (@deal.preferred.to_s == "1" && (@deal.admin_preferred.blank? || @deal.admin_preferred..to_s == '0'))? true : false
+    un_preference = (@deal.preferred.to_s == "1" && (@deal.admin_preferred.blank? || @deal.admin_preferred.to_s == '0'))? true : false
     @deal.update_attributes(:name => params[:name], :value => params[:actual_value], :rules => params[:rules], :highlights => params[:highlights], :expiry_date => Time.parse("#{params[:expiry_date].gsub('/','-')} 23:59:59").to_i.to_s, :commission => params[:commission])
     schedule.update_attributes(:start_time => Time.parse("#{params[:start_date].gsub('/','-')} 00:00:00").to_i.to_s, :end_time => Time.parse("#{params[:close_date].gsub('/','-')} 23:59:59").to_i.to_s)
     if preference
