e88381fe296a4b5ab17616367ed174094ed9cd9c
hibiscustech <relton@hibiscustech.com>
2010-11-26 06:37:36 +0530

    deal creation step2

--- db/migrate/20101126000239_add_new_column_to_deal_discounts.rb
+++ db/migrate/20101126000239_add_new_column_to_deal_discounts.rb
@@ -3,6 +3,9 @@ class AddNewColumnToDealDiscounts < ActiveRecord::Migration
     execute %Q{ ALTER TABLE `deal_discounts` ADD COLUMN `max_customers` INT(11) UNSIGNED AFTER `customers`}
 
     execute %Q{ ALTER TABLE `deals` ADD COLUMN `minimum_number` INT(11) UNSIGNED AFTER `save_amount`}
+
+    execute %Q{ ALTER TABLE `deals` DROP COLUMN `commission`,
+                ADD COLUMN `preferred` ENUM('1','0') NOT NULL DEFAULT '0' AFTER `keupoints_required`}
   end
 
   def self.down
