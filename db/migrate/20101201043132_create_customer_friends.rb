e0ea2a4f31c60520270c46150f2b4c6349336676
Divya <divyakr1985@gmail.com>
2010-12-02 16:24:55 +0530

    customer_friends table

--- db/migrate/20101201043132_create_customer_friends.rb
+++ db/migrate/20101201043132_create_customer_friends.rb
@@ -1,10 +1,10 @@
 class CreateCustomerFriends < ActiveRecord::Migration
   def self.up
-#    create_table :customer_friends do |t|
- #     t.integer :customer_id
-  #    t.string :friend_email
-   #   t.timestamps
-    #end
+    create_table :customer_friends do |t|
+      t.integer :customer_id
+      t.string :friend_email
+      t.timestamps
+    end
   end
 
   def self.down
