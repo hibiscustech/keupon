9b89883dcf3773bbad5fce335dc883c843ccdb93
hibiscustech <relton@hibiscustech.com>
2010-12-02 12:34:51 +0530

    current discount

--- app/models/deal_discount.rb
+++ app/models/deal_discount.rb
@@ -4,6 +4,6 @@ class DealDiscount < ActiveRecord::Base
   def self.deal_current_discount(deal_id, no_of_customers)
     query = %Q{ select discount from deal_discounts where deal_id = #{deal_id} and #{no_of_customers} between customers and max_customers }
     dd = find_by_sql(query)[0]
-    return dd.discount
+    return (dd.blank?)? "0" : dd.discount
   end
 end
