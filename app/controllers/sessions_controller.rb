1a3ab0c219b2f3de604103a566fd4ead4bba5f38
Divya <divyakr1985@gmail.com>
2010-12-02 18:11:42 +0530

    Flash message changed for admin users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,9 +15,9 @@ class SessionsController < ApplicationController
     success = @user && @user.save
     if success && @user.errors.empty?
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "Admin user is created.Please sign in with your login credentials"
     else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      flash[:error]  = "We couldn't set up that account, sorry."
       render :action => 'new', :layout=> 'signup'
     end
 
