b15561c16ac47b9ca822b0ff0cdd5558310096e4
hibiscustech <relton@hibiscustech.com>
2010-12-22 14:52:43 +0530

    new style

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -66,11 +66,11 @@ class DealsController < ApplicationController
   end
 
   def add_new_deal_discount
-    @deal = Deal.find(params[:id])
     discount = params[:discount]
     customers = params[:customer]
     max_customers = params[:max_customer]
-    session[:deal_discounts][discount.to_i] = [customers, max_customers]
+    session[:deal_discounts][discount.to_f] = [customers, max_customers]
+    @deal_scale_xml = deal_scale_graph(session[:deal_discounts].sort)
     if request.xml_http_request?
       respond_to do |format|
         format.html
@@ -79,15 +79,29 @@ class DealsController < ApplicationController
             page.replace_html 'minimum_customer', "<input type='text' name='customer1' value='#{(max_customers.to_i+1).to_s}' disabled/><input type='hidden' name='customer' id='customer' value='#{(max_customers.to_i+1).to_s}'/>"
             page.replace_html 'maximum_customer', "<input type='text' name='max_customer' id='max_customer' />"
             page.replace_html 'disc', "<input type='text' name='discount' id='discount' />"
-            page.replace_html 'dd_form_create', "<a href='#' onclick='return form_validator1(#{discount.to_i});return false;'>Create</a>"
-            page.replace_html 'discount_summary',:partial => "deal_discount_summary"
-            if !session[:deal_discounts].blank?
-              page.replace_html 'ds_form',:partial => "deal_discount_form"
-            end
+            page.replace_html 'discount_summary',:partial => "deal_discount_summary"            
           end
         }
       end
     end
+  end
+
+  def deal_scale_graph(deal_discounts)
+    minimum = deal_discounts[0][1][0]
+    deals_bought = minimum
+    maximum = deal_discounts[deal_discounts.length-1][1][1]
+    customers_discount_ranges = "<colorRange>"
+    prev_max_customers = nil
+    for dd in deal_discounts
+      min_customers = dd[1][0]
+      max_customers = dd[1][1]
+      discount = dd[0]
+      current_min_customers = (prev_max_customers.blank?)? min_customers : prev_max_customers
+      customers_discount_ranges += "<color minValue='#{current_min_customers}' maxValue='#{max_customers}' code='c41111' borderColor='ffffff' label='#{discount}%'/>"
+      prev_max_customers = max_customers
+    end
+    customers_discount_ranges += "</colorRange><pointers><pointer value='#{deals_bought}' bgColor='FFFFFF' radius='5' toolText='Keupons Bought: #{deals_bought}'/></pointers>"
+    return "<chart bgSWF='/images/price_black_bg.jpg' borderColor='DCCEA1' chartTopMargin='0' chartBottomMargin='0' ticksBelowGauge='1' tickMarkDistance='3' valuePadding='-2' majorTMColor='000000' majorTMNumber='3' minorTMNumber='4' minorTMHeight='4' majorTMHeight='8' showShadow='0' gaugeBorderThickness='3' baseFontColor='000000' gaugeFillMix='{color},{FFFFFF}' gaugeFillRatio='50,50' upperLimit='#{maximum}' lowerLimit='#{minimum}'>#{customers_discount_ranges}<styles><definition><style name='limitFont' type='Font' bold='1'/><style name='labelFont' type='Font' bold='1' size='10' color='FFFFFF'/><style name='TTipFont' type='Font' color='FFFFFF' bgColor='000000' borderColor='000000'/></definition><application><apply toObject='GAUGELABELS' styles='labelFont'/><apply toObject='LIMITVALUES' styles='limitFont'/><apply toObject='TOOLTIP' styles='TTipFont'/></application></styles></chart>"
   end
 
   def cancel_new_discount_customers
@@ -142,7 +156,7 @@ class DealsController < ApplicationController
     end
 
     if @deal.save!
-      deal_location = DealLocationDetail.new(:deal_id => @deal.id, :address1 => merchant_profile.address1, :address2 => merchant_profile.address2, :state => merchant_profile.country, :zipcode => merchant_profile.zipcode)
+      deal_location = DealLocationDetail.new(:deal_id => @deal.id, :address1 => params[:address1], :address2 => params[:address2], :state => params[:country], :city => params[:country], :zipcode => params[:zipcode])
       get_lat_lng(deal_location)
       deal_location.save!
       deal_schedule = DealSchedule.new(:deal_id => @deal.id, :start_time => Time.parse("#{params[:start_date].gsub('/','-')} 00:00:00").to_i.to_s, :end_time => Time.parse("#{params[:end_date].gsub('/','-')} 23:59:59").to_i.to_s)
@@ -150,8 +164,26 @@ class DealsController < ApplicationController
       if @deal.preferred.to_s == "1"
         AdminMailer.deliver_merchant_created_preferred_deal(@deal, merchant_profile, merchant_profile.company)
       end
-      session[:deal_discounts] = Hash.new
-      redirect_to "/deals/index?id=#{@deal.id}"
+
+      deal_discounts = session[:deal_discounts].sort
+      min_customers = nil
+      max_customers = nil
+      buy = nil
+      save_amount = nil
+      discount = nil
+
+      for dd in deal_discounts
+        buy = @deal.value.to_f - @deal.value.to_f*dd[0].to_f/100
+        save_amount = @deal.value.to_f - buy.to_f
+        discount = dd[0]
+        min_customers = dd[1][0]
+        max_customers = dd[1][1]
+        DealDiscount.create(:deal_id => params[:deal_id], :discount => discount, :customers => min_customers, :max_customers => max_customers, :buy_value => buy, :save_amount => save_amount)
+      end
+      @deal.update_attributes(:minimum_number => min_customers, :number => max_customers, :buy => buy, :save_amount => save_amount, :discount => discount)
+      session[:deal_discounts] = nil
+
+      redirect_to "/index"
     end
   end
 
