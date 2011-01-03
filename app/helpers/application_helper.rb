19939efac82b7a2073a458e7af30f33b64de98b3
hibiscustech <relton@hibiscustech.com>
2011-01-03 19:15:36 +0530

    new style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,14 +30,14 @@ module ApplicationHelper
   end
 
   def sort_td_class_helper(param)
-    result = "<img src='/images/up_arrow.png' width='15' height='8' />'" if params[:sort] == param
-    result = "<img src='/images/down_arrow.png' width='15' height='8' />'" if params[:sort] == param + "_reverse"
+    result = "<img src='/images/up_arrow.png' width='15' height='8' />" if params[:sort] == param
+    result = "<img src='/images/down_arrow.png' width='15' height='8' />" if params[:sort] == param + "_reverse"
     return result
   end
 
   def sort_admin_td_class_helper(param)
-    result = "<img src='/images/up_arrow_new.png' width='15' height='8' />'" if params[:sort] == param
-    result = "<img src='/images/down_arrow_new.png' width='15' height='8' />'" if params[:sort] == param + "_reverse"
+    result = "<img src='/images/up_arrow_new.png' width='15' height='8' />" if params[:sort] == param
+    result = "<img src='/images/down_arrow_new.png' width='15' height='8' />" if params[:sort] == param + "_reverse"
     return result
   end
 
