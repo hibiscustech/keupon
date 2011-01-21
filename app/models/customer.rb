617c4768ca617e3bf866eda476eea1af8fa7bf56
Divya <divyakr1985@gmail.com>
2011-01-21 13:06:44 +0530

    signup bug fixed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,7 +14,7 @@ class Customer < ActiveRecord::Base
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
+  validates_uniqueness_of   :email,:message=>"Id - is already in use, please use another email"
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   before_create :make_activation_code 
