03ed8fbeedce0a6f989b88ebed22088f4dd7cf08
Divya <divyakr1985@gmail.com>
2011-01-24 12:51:20 +0530

    Foto url small

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -116,7 +116,7 @@ protected
         xml.response 'success'
         xml.user_id customer.id
         xml.login customer.login
-        xml.user_photo customer.customer_photo.url
+        xml.user_photo customer.customer_photo.url(:small)
        end
        respond_to do |format|
          format.xml { render :xml => xml.target! }
