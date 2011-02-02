83b90c86e9b0fc4c81b12ce08d9df64f6aa4d4ca
Divya <divyakr1985@gmail.com>
2011-02-02 12:44:47 +0530

    New API

--- app/models/deal_location_detail.rb
+++ app/models/deal_location_detail.rb
@@ -30,7 +30,7 @@ class DealLocationDetail < ActiveRecord::Base
 
   def self.near_by_deals(latitude, longitude, max_kms)
     query = %Q{
-      select d.id, d.name, c.name as company_name, dld.address1, dld.address2, dld.city, dld.zipcode, (6371*(2*(atan2(sqrt(sin(radians(dld.latitude-#{latitude})/2) * sin(radians(dld.latitude-#{latitude})/2) + cos(radians(#{latitude})) * cos(radians(dld.latitude)) * sin(radians(dld.longitude-#{longitude})/2) * sin(radians(dld.longitude-#{longitude})/2)), sqrt(1-(sin(radians(dld.latitude-#{latitude})/2) * sin(radians(dld.latitude-#{latitude})/2) + cos(radians(#{latitude})) * cos(radians(dld.latitude)) * sin(radians(dld.longitude-#{longitude})/2) * sin(radians(dld.longitude-#{longitude})/2))))))) as distance
+      select d.id,dld.deal_id,d.name, c.name as company_name, dld.address1,dld.latitude, dld.longitude,dld.address2, dld.city, dld.zipcode, (6371*(2*(atan2(sqrt(sin(radians(dld.latitude-#{latitude})/2) * sin(radians(dld.latitude-#{latitude})/2) + cos(radians(#{latitude})) * cos(radians(dld.latitude)) * sin(radians(dld.longitude-#{longitude})/2) * sin(radians(dld.longitude-#{longitude})/2)), sqrt(1-(sin(radians(dld.latitude-#{latitude})/2) * sin(radians(dld.latitude-#{latitude})/2) + cos(radians(#{latitude})) * cos(radians(dld.latitude)) * sin(radians(dld.longitude-#{longitude})/2) * sin(radians(dld.longitude-#{longitude})/2))))))) as distance
       from deal_location_details dld
       join deals d on dld.deal_id = d.id
       join merchant_profiles mp on mp.merchant_id = d.merchant_id
